44b672be9adbe765185e80b01041f0d5865f008b
Amjad Shaaban <theamjad@outlook.com>
2020-01-17 08:13:24 -0500

    Updates

---
+++ precache-manifest.73af7ae7542d408bd577827b9c9e40bb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07a10aa1594f9ad4339935d146fb04d3",
+    "url": "/ReactMemoryGame/client/index.html"
+  },
+  {
+    "revision": "7f9ca3c74f42e520c3b8",
+    "url": "/ReactMemoryGame/client/static/js/2.41cccbf9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/ReactMemoryGame/client/static/js/2.41cccbf9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "22135e0f73c221072817",
+    "url": "/ReactMemoryGame/client/static/js/main.c832bd6f.chunk.js"
+  },
+  {
+    "revision": "eb0fa5ebf6ea47ef8040",
+    "url": "/ReactMemoryGame/client/static/js/runtime-main.e52a61c5.js"
+  }
+]);
